1d25180c70972391fe1be40d0ddca48add0a76ad
unknown <medo39286@gmail.com>
2022-09-26 23:37:51 +0200

    more changes

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
 let inputName = document.querySelector(".input-name");
 let letsGo = document.querySelector(".lets-go-btn");
+let startAud = document.querySelector(".start-audio");
+let startPage = document.querySelector(".start-page");
+let helloTxt = document.querySelector(".hello-txt");
+let readyPage = document.querySelector(".ready-page")
 
-// function checkName(){
-    
-//     if(inputName.value.length != 0){
-//         letsGo.style.display = "flex"
-//     }else{
-//         letsGo.style.display = "none";
-//     }
-// }
-// checkName()
-// console.log(inputName.value)
 
-// check input
+let music = new Audio("Audio/StartSound.mp3");
 
 inputName.oninput = function(){
-    if (inputName.value.length != 0) {
-      letsGo.style.display = "flex";
+  let inv = inputName.value
+    if (inputName.value.length != 0 ) {
+      // letsGo.style.display = "flex";
+      letsGo.style.opacity = "1"
+      letsGo.style.cursor = "pointer"
     } else {
-      letsGo.style.display = "none";
+      letsGo.style.opacity = "0.5"
+      letsGo.style.cursor = "not-allowed"
+      // letsGo.style.display = "none";
     }
 }
-
 // stop form 
-letsGo.onclick = function(){
 
+
+letsGo.addEventListener("click" , function(e){
+  e.preventDefault();
+  let innerVal = inputName.value;
+  if (inputName.value.length != 0 ){
+    console.log(inputName.value);
+    startPage.style.display = "none";
+    readyPage.style.display = "block";
+
+    let i = 0;
+    let helloText = `Hello ${innerVal}, lets Start Our Journey`;
+function typingHello() {
+  if (i < helloText.length) {
+    helloTxt.innerHTML += helloText.charAt(i);
+    i++;
+    setTimeout(typingHello, 60);
+  }
 }
+typingHello()
+    music.play()
+  }else{
+    startPage.style.display = "block";
+  }
+})
